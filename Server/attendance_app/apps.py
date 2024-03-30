bc31e8359310d68999029fb7076e268bdf3f2e59
stabem <gabrielstabelini22@gmail.com>
2024-03-30 17:46:29 -0300

    add django, endpoints: recordAttendance and listStudents

--- Server/attendance_app/apps.py
+++ Server/attendance_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class AttendanceAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
+    default_auto_field = "Server.db.models.BigAutoField"
     name = "attendance_app"
