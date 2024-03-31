11663e278b79d1d28d90607de0793a8084632024
stabem <gabrielstabelini22@gmail.com>
2024-03-31 19:16:04 -0300

    alteracao nos endpoints

--- Server/attendance_app/views.py
+++ Server/attendance_app/views.py
@@ -18,39 +18,46 @@ class ClassAttendanceView(APIView):
 
 class AttendanceRecordView(APIView):
     def get(self, request):
-        # Construindo o filtro com base nos parâmetros opcionais
-        period = request.query_params.get('period')
-        schoolGrade = request.query_params.get('schoolGrade')
-        classMeta = request.query_params.get('classMeta')
-        teacher_id = request.query_params.get('teacher')
-        date = request.query_params.get('date')
+        # Capturando os parâmetros da query
+        period = request.query_params.get('period', None)
+        schoolGrade = request.query_params.get('schoolGrade', None)
+        classMeta = request.query_params.get('classMeta', None)
+        teacher_id = request.query_params.get('teacher', None)
+        date = request.query_params.get('date', None)
 
-        # Encontrando as classes específicas com os filtros aplicados
-        class_instance = get_object_or_404(Class,
-                                           period=period,
-                                           schoolGrade=schoolGrade,
-                                           classMeta=classMeta,
-                                           teacher_id=teacher_id)
+        # Construindo os filtros com base nos parâmetros fornecidos
+        filters = {}
+        if period:
+            filters['period'] = period
+        if schoolGrade:
+            filters['schoolGrade'] = schoolGrade
+        if classMeta:
+            filters['classMeta'] = classMeta
+        if teacher_id:
+            filters['teacher_id'] = teacher_id
+
+        # Encontrando a classe específica (assumindo que ela é única)
+        class_instance = get_object_or_404(Class, **filters)
 
         # Filtrando registros de presença por data e classe
         attendance_records = AttendanceRecord.objects.filter(classMeta=class_instance, date=date)
-        student_ids = attendance_records.values_list('student', flat=True)
-        students = Student.objects.filter(id__in=student_ids)
 
         # Preparando a lista de estudantes para a resposta
-        students_list = [{
-            "student": attendance_record.student.id,
-            "attending": attendance_record.attending
-        } for attendance_record in attendance_records]
+        students_list = [
+            {
+                "student": attendance_record.student.name,
+                "attending": attendance_record.attending
+            } for attendance_record in attendance_records
+        ]
 
         # Preparando os dados da resposta
         response_data = {
-            "period": period,
-            "schoolGrade": schoolGrade,
-            "classMeta": classMeta,
-            "teacher": teacher_id,
+            "period": class_instance.period,
+            "schoolGrade": class_instance.schoolGrade,
+            "classMeta": class_instance.classMeta,
+            "teacher": class_instance.teacher.id,
             "date": date,
             "studentsList": students_list
         }
 
-        return Response(response_data, status.HTTP_201_CREATED)
+        return Response(response_data)
