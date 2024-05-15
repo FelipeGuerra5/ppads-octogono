3645d490384172456f430463a28908ad32b9447b
stabem <gabrielstabelini22@gmail.com>
2024-05-15 13:48:26 -0300

    json data

--- Server/attendance_app/urls.py
+++ Server/attendance_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ClassAttendanceView, ClassStatisticsView, StudentStatisticsView, AttendanceRecordView
 
 urlpatterns = [
-    path('attendance/', ClassAttendanceView.as_view(), name='class-attendance'),
+    path('attendance/<int:class_id>/attendance/', ClassAttendanceView.as_view(), name='class-attendance'),
     path('<int:class_id>/statistics/', ClassStatisticsView.as_view(), name='class-statistics'),
-    path('<int:student_id>/statistics/', StudentStatisticsView.as_view(), name='student-statistics'),
+    path('student/<int:student_id>/statistics/', StudentStatisticsView.as_view(), name='student-statistics'),
     path('attendance-records/', AttendanceRecordView.as_view(), name='attendance-records'),
 ]
