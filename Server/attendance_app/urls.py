6b446a69ce0795ed8a4ef0b39b4bcbcf124e5179
stabem <gabrielstabelini22@gmail.com>
2024-05-15 13:51:16 -0300

    url

--- Server/attendance_app/urls.py
+++ Server/attendance_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ClassAttendanceView, ClassStatisticsView, StudentStatisticsView, AttendanceRecordView
 
 urlpatterns = [
-    path('attendance/<int:class_id>/attendance/', ClassAttendanceView.as_view(), name='class-attendance'),
+    path('<int:class_id>/attendance/', ClassAttendanceView.as_view(), name='class-attendance'),
     path('<int:class_id>/statistics/', ClassStatisticsView.as_view(), name='class-statistics'),
     path('student/<int:student_id>/statistics/', StudentStatisticsView.as_view(), name='student-statistics'),
     path('attendance-records/', AttendanceRecordView.as_view(), name='attendance-records'),
